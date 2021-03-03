4693575d2c2b8c7437e9b64d4fbf2d995e37fb42
Malek KAZDZAGHLI <malek.kazdaghli@sofrecom.com>
2021-03-03 23:21:31 +0100

    added logging to feed.router.ts

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -36,6 +36,7 @@ router.get('/', async (req: Request, res: Response) => {
       item.url = AWS.getGetSignedUrl(item.url);
     }
   });
+  console.log("INFO - Get all feed items request called")
   res.send(items);
 });
 
